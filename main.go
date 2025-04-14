717b307bb46c6683cc3866879b87a3a80802c9c8
IonovEgor <ionov.egor@gmail.com>
2025-04-14 14:39:29 +0300

    final solution v1

--- main.go
+++ main.go
@@ -9,32 +9,23 @@ import (
 
 func init() {
 	_ = godotenv.Load()
-	//ToDo написать обработчик ошибок или логирование
 }
 
 func main() {
 	err := db.Init(config.GetEnv("TODO_DBFILE", "scheduler.db"))
 	if err != nil {
-		//ToDo написать обработчик ошибок или логирование
 		return
 	}
 	defer func() {
 		err := db.CloseDB()
 		if err != nil {
-			//ToDo написать обработчик ошибок или логирование
 			return
 		}
 	}()
 
 	err = server.Run()
 	if err != nil {
-		//ToDo написать обработчик ошибок или логирование
 		return
 	}
 
 }
-
-//ToDo
-//1. Необходимо завести сокращенную ссылку из задания "Запуская web-server" в go.mod и заменить длинные адреса пакетов
-//replace go1f => ./
-//require go1f v0.0.0
